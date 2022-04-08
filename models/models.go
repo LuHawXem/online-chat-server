254abe58ef961916f4d425d8e729458e7e7c4dad
草帽 <1906100003@e.gzhu.edu.cn>
2022-04-09 03:09:29 +0800

    version:又不是不能用.jpg 版,基本的加好友和聊天逻辑已经有了

--- models/models.go
+++ models/models.go
@@ -10,6 +10,7 @@ type TUser struct {
 	Account   uint64      `json:"account" gorm:"bigint"`
 	Password  string      `json:"password" gorm:"varchar"`
 	RoleID    uint8       `json:"role_id,omitempty" gorm:"tinyint;default:0" remark:"0表示普通用户"`
+	Gender	  null.Int	  `json:"gender,omitempty" gorm:"tinyint" remark:"性别,0男,1女"`
 	Nickname  null.String `json:"nickname,omitempty" gorm:"varchar"`
 	Avatar    null.String `json:"avatar,omitempty" gorm:"varchar"`
 	Country   null.String `json:"country,omitempty" gorm:"varchar"`
@@ -27,6 +28,7 @@ type User struct {
 	ID       uint32      `json:"id,omitempty" gorm:"primary_key"`
 	Account  uint64      `json:"account" gorm:"bigint"`
 	RoleID   uint8       `json:"role_id,omitempty" gorm:"tinyint;default:0" remark:"0表示普通用户"`
+	Gender	  null.Int	  `json:"gender,omitempty" gorm:"tinyint" remark:"性别,0男,1女"`
 	Nickname null.String `json:"nickname,omitempty" gorm:"varchar"`
 	Avatar   null.String `json:"avatar,omitempty" gorm:"varchar"`
 	Country  null.String `json:"country,omitempty" gorm:"varchar"`
@@ -40,7 +42,7 @@ type TRelation struct {
 	UserID    uint32      `json:"user_id,omitempty" gorm:"int" remark:"关系中对方的UserID"`
 	Remark    null.String `json:"remark,omitempty" gorm:"varchar" remark:"关系中对对方的备注"`
 	CreatedBy uint32      `json:"created_by,omitempty" gorm:"int" remark:"关系中己方的UserID"`
-	State     uint8       `json:"state" gorm:"tinyint;default:1" remark:"关系的状态,1表示发起中,0表示双向已建立,2表示单向(己方->对方)已建立"`
+	State     uint8       `json:"state" gorm:"tinyint;default:1" remark:"关系的状态,0表示双向已建立,1表示单向(己方->对方)已建立"`
 	CreatedAt time.Time   `json:"created_at,omitempty"`
 	UpdatedAt time.Time   `json:"updated_at,omitempty"`
 	Status 	  uint8		  `json:"status,omitempty" gorm:"tinyint;default:0" remark:"数据的状态,0表示存在,1表示已删除,可扩展"`
@@ -65,5 +67,5 @@ type Message struct {
 	ReplyID	  null.Int    `json:"reply_id,omitempty" remark:"回复消息的id,可能没有"`
 	Operate	  null.Int	  `json:"operate,omitempty" remark:"操作,如回复消息(存在ReplyID且不存在Operate即为回复),通过请求(1)与否(0)"`
 	Content   null.String `json:"content,omitempty" remark:"正文,可能没有"`
-	Type      uint8 	  `json:"type" remark:"消息类型,0普通消息(对单),1加好友(对单),2群消息,3加群,4搜索用户"`
+	Type      uint8 	  `json:"type" remark:"消息类型,0普通消息(对单),1加好友(对单),2群消息,3加群,4搜索用户,5获取好友列表,200心跳包"`
 }
--- service/websocket.go
+++ service/websocket.go
@@ -53,17 +53,54 @@ func ReadMessage(ws *websocket.Conn, id uint32, key string) {
 
 		switch m.Type {
 		case 0:
+			if m.ReplyID.Valid && m.Operate.Valid {
+				var r models.TMessage
+				models.GetDB().Where("id = ?", m.ReplyID).First(&r)
+				models.GetDB().Model(&r).Update("state", 2)
+				if m.Operate.Int64 == 1 {
+					models.GetDB().Create(&models.TRelation{
+						UserID:    r.CreatedBy,
+						CreatedBy: id,
+						State:     0,
+					})
+					models.GetDB().Create(&models.TRelation{
+						UserID:    id,
+						CreatedBy: r.CreatedBy,
+						State:     0,
+					})
+				}
+			} else {
+				message := models.TMessage{
+					Receiver:  m.Receiver,
+					ReplyID:   m.ReplyID,
+					Operate:   m.Operate,
+					Content:   m.Content,
+					Type:      m.Type,
+					State:     0,
+					CreatedBy: id,
+				}
+				models.GetDB().Create(&message)
+			}
 			break
 		case 1:
+			var r models.TMessage
+			models.GetDB().Where("receiver = ? and type = ? and created_by = ?", m.Receiver, m.Type, id).First(&r)
+			if r.ID != 0 {
+				ws.WriteJSON(gin.H{
+					"msg": "repeat request",
+					"status": http.StatusBadRequest,
+				})
+				break
+			}
 			message := models.TMessage{
 				Receiver:  m.Receiver,
-				ReplyID:   m.ReplyID,
-				Operate:   m.Operate,
-				Content:   m.Content,
 				Type:      m.Type,
 				CreatedBy: id,
 			}
 			models.GetDB().Create(&message)
+			ws.WriteJSON(gin.H{
+				"status": http.StatusOK,
+			})
 			break
 		case 2:
 			break
@@ -81,7 +118,35 @@ func ReadMessage(ws *websocket.Conn, id uint32, key string) {
 					delete(conn, key)
 					goto exit
 				}
+			} else {
+				ws.WriteJSON(gin.H{
+					"msg": "user not found",
+					"status": http.StatusNoContent,
+				})
 			}
+			break
+		case 5:
+			var friends []models.TRelation
+			models.GetDB().Where("created_by = ?", id).Find(&friends)
+			var data []gin.H
+			for _, v := range friends {
+				var u models.User
+				models.GetDB().Table("t_user").Where("id = ?", v.UserID).First(&u)
+				data = append(data, gin.H{
+					"user": u,
+					"relation": v,
+				})
+			}
+			ws.WriteJSON(gin.H{
+				"type": 5,
+				"friends": data,
+				"status": http.StatusOK,
+			})
+			break
+		case 200:
+			ws.WriteJSON(gin.H{
+				"status": http.StatusOK,
+			})
 			break
 		}
 	}
@@ -94,7 +159,21 @@ func WriteMessage(ws *websocket.Conn, id uint32, key string) {
 		models.GetDB().Where("receiver = ? AND state = 0", id).Find(&messages)
 		if len(messages) != 0 {
 			for _, message := range messages {
-				err := ws.WriteJSON(message)
+				var err error
+				if message.Type == 1 {
+					var user models.User
+					models.GetDB().Table("t_user").Where("id = ?", message.CreatedBy).First(&user)
+					err = ws.WriteJSON(gin.H{
+						"id": message.ID,
+						"type": message.Type,
+						"created_by": message.CreatedBy,
+						"content": message.Content,
+						"nickname": user.Nickname,
+						"avatar": user.Avatar,
+					})
+				} else {
+					err = ws.WriteJSON(message)
+				}
 				if err != nil {
 					log.Printf("chain %s ReadJSON error: %v\n", key, err)
 					ws.Close()
